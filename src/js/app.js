3fce0322a5112dae926ea1b1ccdefe032d784633
Mihir Chaturvedi <plibither8@gmail.com>
2018-03-27 13:29:44 +0530

    Accept trailing slash and redirect to without-slash URL

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,20 @@
 
 	routerApp.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
+		$urlRouterProvider.rule(function($injector, $location) {
+
+			var path = $location.path();
+			var hasTrailingSlash = path[path.length-1] === '/';
+		
+			if(hasTrailingSlash) {
+		
+			  //if last charcter is a slash, return the same url without the slash  
+			  var newPath = path.substr(0, path.length - 1); 
+			  return newPath; 
+			} 
+		
+		  });
+
 		$urlRouterProvider.otherwise('/404');
 
 		$stateProvider
